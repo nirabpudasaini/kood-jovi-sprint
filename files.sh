086f32de6a0f6398cb06854d93b0e006fcd86793
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-05 16:54:01 +0300

    fixes issue in conditions

--- files.sh
+++ files.sh
@@ -2,10 +2,10 @@
 touch a \! \\ \"
 mkdir \`
 mv \! \`/\!
-if  [ MOVE_A == 'yes' ]; then
- mv a \`/a
- elif [ MOVE_A == 'no' ]; then
-   rm a
+if  [ "$MOVE_A" == yes ]; then
+    mv a \`/a
+ elif [ "$MOVE_A" == no ]; then
+    rm a
  else
-   echo "MOVE_A variable not recognized"
+    echo "MOVE_A variable not recognized"
 fi
