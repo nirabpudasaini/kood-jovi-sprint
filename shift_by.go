bc9a6230c01cf03f1b97ab4d6b1ba693267a3229
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-06 11:41:51 +0300

    fix logic error in looping back

--- shift_by.go
+++ shift_by.go
@@ -3,7 +3,7 @@ package sprint
 func ShiftBy(r rune, step int) rune {
 	shifted := rune(int(r) + step)
 	for shifted > 122 {
-		shifted = 97 + (shifted - 122)
+		shifted = 96 + (shifted - 122)
 	}
 	return shifted
 }
