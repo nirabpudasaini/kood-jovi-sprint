80e4139438faa44f2eab483702e7df1b78b19dd7
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-08 11:19:19 +0300

    output correct formatting

--- pairs.go
+++ pairs.go
@@ -5,11 +5,8 @@ import "fmt"
 func Pairs() string {
 	result := ""
 	for i := 0; i <= 99; i++ {
-		for j := 1; j <= 99; j++ {
-			if i == j {
-				continue
-			}
-			if i == 99 && j == 98 {
+		for j := i + 1; j <= 99; j++ {
+			if i == 98 && j == 99 {
 				result += fmt.Sprintf("%02d %02d", i, j)
 				continue
 			}
