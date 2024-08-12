87aaecb78bd7a1f799199f2c1c9e10d580536009
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-12 10:47:02 +0300

    removes redudant code from str to int

--- simple_str_to_int.go
+++ simple_str_to_int.go
@@ -3,8 +3,7 @@ package sprint
 func SimpleStrToInt(s string) int {
 	var result int
 	for _, i := range []byte(s) {
-		if i >= '0' && i <= '9' {
-		} else {
+		if i < '0' && i > '9' {
 			return 0
 		}
 		result = result*10 + int(i-'0')
