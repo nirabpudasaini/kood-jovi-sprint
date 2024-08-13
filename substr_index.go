472663c474a7ed6004a503ef4fd2419722de0183
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-13 16:24:55 +0300

    add edge case to substr index

--- substr_index.go
+++ substr_index.go
@@ -4,6 +4,12 @@ func SubstrIndex(s, toFind string) int {
 	sArr := []rune(s)
 	sLen := len(sArr)
 	toFindLen := len(toFind)
+	if toFind == "" {
+		return 0
+	}
+	if s == toFind {
+		return 0
+	}
 	for i := 0; i < sLen; i++ {
 		if s[i] == toFind[0] {
 			if i+toFindLen < sLen {
