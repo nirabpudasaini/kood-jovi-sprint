89d16e6373bef95036c1f13c4223180700d8a773
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-13 11:44:40 +0300

    add str concat with seperator

---
+++ str_concat_with.go
@@ -0,0 +1,14 @@
+package sprint
+
+func StrConcatWith(strs []string, sep string) string {
+	result := ""
+	lastIndex := len(strs) - 1
+	for i, v := range strs {
+		if i == lastIndex {
+			result += v
+			break
+		}
+		result = result + v + sep + " "
+	}
+	return result
+}
