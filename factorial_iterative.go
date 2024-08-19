7fe533e42776318dc93739d4158d9704ad44da33
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-19 14:45:14 +0300

    handle negative

--- factorial_iterative.go
+++ factorial_iterative.go
@@ -1,9 +1,12 @@
 package sprint
 
 func FactorialIterative(n int) int {
+	if n <= 0 {
+		return 0
+	}
 	factorial := 1
 	for i := 1; i <= n; i++ {
-		factorial *= i
+		factorial = int(uint(i * factorial))
 	}
 	return factorial
 }
