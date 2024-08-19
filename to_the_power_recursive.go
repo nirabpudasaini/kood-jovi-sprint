c8404ff776b5a4101c24f5d4df74d4290db4f010
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-19 15:39:21 +0300

    add power recursive

---
+++ to_the_power_recursive.go
@@ -0,0 +1,11 @@
+package sprint
+
+func ToThePowerRecursive(n int, power int) int {
+	if power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
+	return n * ToThePowerRecursive(n, power-1)
+}
