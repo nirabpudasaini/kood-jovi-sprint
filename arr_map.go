441f9896ad75c2cec0d3ff39d7beaf6fa4c58593
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-20 16:06:56 +0300

    add arr map

---
+++ arr_map.go
@@ -0,0 +1,28 @@
+package sprint
+
+func ArrMap(f func(int) bool, a []int) []bool {
+	result := []bool{}
+	for _, v := range a {
+		result = append(result, f(v))
+	}
+	return result
+}
+
+func IsPrime(n int) bool {
+	if n == 1 {
+		return false
+	}
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsNegative(n int) bool {
+	return n < 0
+}
