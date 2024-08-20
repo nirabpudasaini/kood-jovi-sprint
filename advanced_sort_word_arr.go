28864ad9d644e6dae3d53bcbdc67c595a1ed1332
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-20 17:05:30 +0300

    remove redudancy

--- advanced_sort_word_arr.go
+++ advanced_sort_word_arr.go
@@ -10,13 +10,3 @@ func AdvancedSortWordArr(a []string, f func(a, b string) int) []string {
 	}
 	return a
 }
-
-func StrCompare(a, b string) int {
-	if a == b {
-		return 0
-	} else if a < b {
-		return -1
-	} else {
-		return 1
-	}
-}
