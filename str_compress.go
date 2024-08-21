273b11af3f78efd0a7348c1fc792e0d3c40f1025
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-21 12:10:31 +0300

    add str compress

---
+++ str_compress.go
@@ -0,0 +1,29 @@
+package sprint
+
+import "fmt"
+
+func StrCompress(s string) string {
+	lastLetter := s[0]
+	consucutiveCount := 1
+	result := ""
+	for i := 1; i < len(s); i++ {
+		if s[i] == lastLetter {
+			consucutiveCount++
+		} else {
+			if consucutiveCount == 1 {
+				result = result + string(s[i-1])
+			} else {
+				result = result + fmt.Sprintf("%d%s", consucutiveCount, string(s[i-1]))
+				consucutiveCount = 1
+			}
+		}
+		lastLetter = s[i]
+	}
+	if consucutiveCount == 1 {
+		result = result + string(lastLetter)
+	} else {
+		result = result + fmt.Sprintf("%d%s", consucutiveCount, string(lastLetter))
+	}
+
+	return result
+}
