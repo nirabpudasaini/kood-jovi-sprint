7a648258b805804a799e038d9c62e54c9cde146e
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-21 11:25:26 +0300

    additional check if first letter is equal

--- sort_word_arr.go
+++ sort_word_arr.go
@@ -5,6 +5,10 @@ func SortWordArr(a []string) []string {
 		for j := 0; j < len(a)-i-1; j++ {
 			if rune(a[j][0]) > rune(a[j+1][0]) {
 				a[j], a[j+1] = a[j+1], a[j]
+			} else if rune(a[j][0]) == rune(a[j+1][0]) {
+				if rune(a[j][1]) > rune(a[j+1][1]) {
+					a[j], a[j+1] = a[j+1], a[j]
+				}
 			}
 		}
 	}
