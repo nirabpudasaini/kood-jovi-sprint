7cbd831e8306f75a5588c18af53f0c7bc51fc113
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-21 13:57:09 +0300

    add payout

---
+++ payout.go
@@ -0,0 +1,27 @@
+package sprint
+
+func Payout(amount int, denominations []int) (payout []int) {
+	payout = []int{}
+	denominations = sortDescending(denominations)
+	for amount > 0 {
+		for i := 0; i < len(denominations); i++ {
+			if amount >= denominations[i] {
+				payout = append(payout, denominations[i])
+				amount -= denominations[i]
+				break
+			}
+		}
+	}
+	return sortDescending(payout)
+}
+
+func sortDescending(arr []int) []int {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := 0; j < len(arr)-i-1; j++ {
+			if arr[j] < arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
