2aa7d92dfaab3882d368b356a9f13704fc93563b
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-21 10:24:53 +0300

    remove duplicates

--- is_palindrome.go
+++ is_palindrome.go
@@ -3,12 +3,3 @@ package sprint
 func IsPalindrome(s string) bool {
 	return s == StrReverse(s)
 }
-
-func StrReverse(s string) string {
-	sRuneArray := []rune(s)
-	result := ""
-	for i := len(sRuneArray) - 1; i >= 0; i-- {
-		result += string(sRuneArray[i])
-	}
-	return result
-}
