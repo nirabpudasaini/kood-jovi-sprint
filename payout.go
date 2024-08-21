60609d34b55a19e7c870238d7c162788acf48843
Nirab Pudasaini <developer.nirab@gmail.com>
2024-08-21 14:32:59 +0300

    handle edege case

--- payout.go
+++ payout.go
@@ -4,6 +4,9 @@ func Payout(amount int, denominations []int) (payout []int) {
 	payout = []int{}
 	denominations = sortDescending(denominations)
 	for amount > 0 {
+		if amount < denominations[len(denominations)-1] {
+			return []int{}
+		}
 		for i := 0; i < len(denominations); i++ {
 			if amount >= denominations[i] {
 				payout = append(payout, denominations[i])
